847c7dc6fada5fbebb61b4d74e1696a5f5fbe499
Bruno Trindade <btrindade172@gmail.com>
2026-05-30 10:42:58 -0300

    feat: fallback to non-existent routes (404)

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { type Request, type Response } from 'express';
 import scheduleRoutes from './routes/schedule.routes.js';
 import cors from 'cors';
 
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', scheduleRoutes);
+
+app.use((_req: Request, res: Response) => {
+    res.status(404).json({ error: "Rota não encontrada. Verifique o endpoint e o método HTTP." });
+});
 
 app.listen(PORT, () => {
     console.log(`Application is running on port: ${PORT}`);
